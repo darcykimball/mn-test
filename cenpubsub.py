3ee0377a881a8e9692a42ac2ecd2b1d67912334a
Kevin Bao <kevinzbao@gmail.com>
2018-03-10 23:26:41 +0000

    Fleshed out more stuff

--- cenpubsub.py
+++ cenpubsub.py
@@ -1,30 +1,98 @@
 #!/usr/bin/python
 
 
+import json
+
+
 # Centralized pub/sub
 
 
+# Some constants
+
 CPUBSUB_PORT = 8888
+
+SUB_MSG_KEY = 'SUB'
+PUB_MSG_KEY = 'PUB'
+EVENT_MSG_KEY = 'EVENT'
+
+NO_SUB_ID = -1 # Special id value for new subscription requests
+
+ID_KEY = 'ID'
+TOPICS_KEY = 'TOPICS'
+
 
 
 # Message types
+# Every message is just JSON
 
-class SubscribeMsg(topics):
+
+
+class SubscribeMsg():
     '''
-    A subscription request message
+    A subscription request message.
+
+    This serves as both a 'join' request and/or a subscribe one.
     '''
-    pass
+
+    
+    def __init__(self, topics, sub_id=NO_SUB_ID):
+        assert len(topics) > 0
+        self.sub_id = sub_id    
+        self.topics = topics
+
+    
+    def to_json(self):
+        d = {
+            SUB_MSG_KEY: {
+                ID_KEY: self.sub_id,
+                TOPICS_KEY: self.topics
+            }
+        }
+        
+        return json.dumps(d, indent=4)
 
 
-def PublishMsg(topic, msg):
+class PublishMsg():
     '''
     A publish message
     '''
-    pass
 
     
-def EventMsg(topic, msg):
+    def __init__(self, topic, msg, pub_id):
+        self.pub_id = pub_id
+        self.topics = [topic]
+        self.msg = msg
+
+    
+    def to_json(self):
+        d = {
+            SUB_MSG_KEY: {
+                ID_KEY: self.pub_id,
+                TOPICS_KEY: self.topics
+            }
+        }
+
+        return json.dumps(d, indent=4)
+
+
+class EventMsg():
     '''
     An event (i.e. forwarded published msg) message
     '''
-    pass
+    
+
+    def __init__(self, topic, msg, broker_id):
+        self.broker_id = broker_id
+        self.topics = [topic]
+        self.msg = msg
+
+    
+    def to_json(self):
+        d = {
+            EVENT_MSG_KEY: {
+                ID_KEY: self.broker_id,
+                TOPICS_KEY: self.topics
+            }
+        }
+
+        return json.dumps(d, indent=4)
--- subscriber.py
+++ subscriber.py
@@ -5,6 +5,8 @@ import argparse
 import socket
 import sys
 
+import cenpubsub
+
 
 class Subscriber:
     '''
@@ -13,21 +15,56 @@ class Subscriber:
 
 
     def __init__(self, broker_ip, port, topics, recv_action=print):
+        self.sub_id = NO_SUB_ID
         self.broker_ip = broker_ip
         self.broker_port = port
         self.topics = topics
         self.recv_action = recv_action
-        self.sock = None
 
 
-    def start():
-        # TODO wrap in try block
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((args.broker_ip, args.port))
+    def join(self):
+        '''
+        Try to join a pub/sub network
+        '''
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.broker_ip, self.broker_port))
+
+        sub_msg = SubscribeMsg(self.sub_id, topics)
+        s.send(sub_msg.to_json())
+
+        # See if we were able to join
+        s.settimeout(2) # Timeout after waiting a bit
+        reply = s.recv()
+        s.settimeout(None)
+
+        if len(reply) == 0:
+            raise UnableToJoinError('No reply from server')
+
+        # FIXME: exception-safe socket close?
+
+
+        # FIXME remove
+        print 'reply: \n' + reply
+        return reply
+
+
+    def start(self):
+        # Try to join as a subscriber
+        reply = self.join()
+        msg = json.loads(reply)
+
+        assert msg[EVENT_MSG_KEY] is not None
+        fields = msg[EVENT_MSG_KEY]
+
+        assert fields[TOPICS_KEY] is not None
+        assert fields[MSG_KEY] is not None
+        self.recv_action(msg)
+
 
     
-    def stop():
-        
+    class UnableToJoinError(Exception):
+        pass
 
 
 if __name__ == '__main__':
@@ -37,11 +74,9 @@ if __name__ == '__main__':
     parser.add_argument('topics', type=str, nargs='+')
 
     args = parser.parse_args()
-    print args # FIXME remove
 
 
     # Run as a listening subscriber
-    sub = Subsc
-    riber((args.broker_ip, args.port))
+    sub = Subscriber(args.broker_ip, args.port, args.topics)
 
-
+    sub.start()
